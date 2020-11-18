ad4b8d8a50903f088ef83ab113ad926274c0fa44
Aturinda Ignatius <ignatius.aturinda@gmail.com>
2020-11-18 14:28:12 +0300

    more on header for large screen

--- resources/views/layouts/user/header.blade.php
+++ resources/views/layouts/user/header.blade.php
@@ -23,6 +23,7 @@
                 font-size: 0.9vw;
                 margin: 0 10px;
                 color: black;
+                background-color: white;
             }
             .col-lg-1, .col-lg-10, .col-lg-11, .col-lg-12, .col-lg-2, .col-lg-3, .col-lg-4, .col-lg-5, .col-lg-6, .col-lg-7, .col-lg-8, .col-lg-9, .col-md-1, .col-md-10, .col-md-11, .col-md-12, .col-md-2, .col-md-3, .col-md-4, .col-md-5, .col-md-6, .col-md-7, .col-md-8, .col-md-9, .col-sm-1, .col-sm-10, .col-sm-11, .col-sm-12, .col-sm-2, .col-sm-3, .col-sm-4, .col-sm-5, .col-sm-6, .col-sm-7, .col-sm-8, .col-sm-9, .col-xs-1, .col-xs-10, .col-xs-11, .col-xs-12, .col-xs-2, .col-xs-3, .col-xs-4, .col-xs-5, .col-xs-6, .col-xs-7, .col-xs-8, .col-xs-9 {
     padding-right: 0px;
@@ -51,7 +52,23 @@
     vertical-align: middle;
 }
 
+    #custom-search-input{
+        width: 92.4%; margin-top: -4px; margin-left: 26px; float: left;
+    }
 
+
+.find{
+        background-color: #36B3BC; padding: 1px 12px; margin-top: -1px; left: -38px; border-radius: 50px; color: #fff; height: 20px; z-index: 9;font-size: 10px;
+    }
+
+@media (min-width: 1680px) and (max-width: 1919px) {
+    .find{
+        background-color: #36B3BC; padding: 1px 12px; margin-top: -2px; left: -38px; border-radius: 50px; color: #fff; height: 30px; z-index: 9;font-size: 10px;
+    }
+    #custom-search-input{
+        width: 90%; margin-top: -4px; margin-left: 30px; float: left;
+    }
+}
 
           
 </style>
@@ -113,13 +130,13 @@
 
         <div class="col-lg-5 col-md-5 col-sm-5 col-xs-12 hidden-xs visible-sm visible-md visible-lg"> 
         <div class="row"> 
-        <div id="custom-search-input" style="width: 92.4%; margin-top: -4px; margin-left: 26px; float: left;" class="">
+        <div id="custom-search-input"  class="">
                     <form method="post" action="{{route('search-all')}}" id="userSearch">
                         <div class="input-group search-input">
                             
                                 <input type="text" id="auto_complete_search" name="key" class="search-query form-control" required placeholder="What are you interested in learning?" style="background-color: white; border-radius: 50px !important;font-size: 10px;" />
                                 <div class="input-group-btn">
-                                    <button class="btn btn-primary" type="submit" style="background-color: #36B3BC; padding: 1px 12px; margin-top: -1px; left: -38px; border-radius: 50px; color: #fff; height: 20px; z-index: 9;font-size: 10px;">
+                                    <button class="btn btn-primary find" type="submit" style="">
                                     Find Lessons
                                     </button>
                                 </div>
